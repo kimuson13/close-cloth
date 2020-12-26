622c0202f1eb55ef004ef9ccf0de0146b0c9a9b3
kimuson <kimuson13@basket.com>
2020-12-26 21:29:49 +0900

    import something

---
+++ cloth_manage/clothes/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('top/<int:num>', views.top, name='top'),
+    path('detail', views.top, name='detail'),
+    path('post', views.post, name='post'),
+    path('edit/<int:num>', views.edit, name='edit'),
+    path('delete/<int:num>', views.delete, name='delete'),
+    path('wishlist', views.wishlist, name='wishlist'),
+    path('logout', views.logout, {'template_name': 'index.html'}, name='logout'),
+]
--- cloth_manage/clothes/views.py
+++ cloth_manage/clothes/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.views.generic import CreateView
+from . forms import UserCreateForm, PostForm, WantedForm
+
 
 # Create your views here.
