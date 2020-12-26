8d5cb594625c333b9d0346985011b0aeb913948f
kimuson <kimuson13@basket.com>
2020-12-26 22:31:00 +0900

    create userform

--- cloth_manage/clothes/views.py
+++ cloth_manage/clothes/views.py
@@ -6,3 +6,18 @@ from . forms import UserCreateForm, PostForm, WantedForm
 
 
 # Create your views here.
+class Create_account(CreateView):
+    def post(self, request, *args, **kwargs):
+        form = UserCreateForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+        return render(request, 'signup.html', {'form': form,})
+    def get(self, request, *args, **kwargs):
+        form = UserCreateForm(request.POST)
+        return render(request, 'signup.html', {'form':form,})
+create_account = Create_account.as_view()
