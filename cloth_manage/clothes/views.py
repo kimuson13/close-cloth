4b426fac0cabb1e12b02bc4c13bc6e055b4fab5c
kimuson <kimuson13@basket.com>
2020-12-29 17:27:16 +0900

    create login-views

--- cloth_manage/clothes/views.py
+++ cloth_manage/clothes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
 from django.views.generic import CreateView
-from . forms import UserCreateForm, PostForm, WantedForm
+from . forms import UserCreateForm, PostForm, WantedForm, LoginForm
 
 
 # Create your views here.
@@ -21,3 +21,18 @@ class Create_account(CreateView):
         form = UserCreateForm(request.POST)
         return render(request, 'signup.html', {'form':form,})
 create_account = Create_account.as_view()
+
+class Account_Login(View):
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            user = User.objects.get(username=username)
+            login(request, user)
+            return redirect(to='/top/')
+        return render(request, 'signin.html', {'form': form,})
+    def get(self, request, *arg, **kwargs):
+        form = LoginForm(request.POST)
+        return render(request, 'signin.html', {'form': form,})
+account_login = Account_Login.as_view()
+
