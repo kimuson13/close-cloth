f2a2a56181707b1f7bd7392e344926b4d5099aad
kimuson <kimuson13@basket.com>
2021-01-03 12:44:50 +0900

    fix images

--- cloth_manage/cloth_manage/urls.py
+++ cloth_manage/cloth_manage/urls.py
@@ -21,6 +21,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clothes/', include('clothes.urls')),
-] 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
